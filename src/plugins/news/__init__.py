d93238528c648ae0a1a88a690724c4330430ec07
lzs <lzs001@foxmail.com>
2021-01-08 17:42:17 +0800

    add news

--- src/plugins/news/__init__.py
+++ src/plugins/news/__init__.py
@@ -1,11 +1,7 @@
 from nonebot import on_command
 from nonebot.adapters.cqhttp import Bot, Event
-# import urllib.request
 import requests
-# import sys
-# import importlib
-import json
-# importlib.reload(sys)
+from bs4 import BeautifulSoup
 
 news = on_command("新闻", priority=1, block=True)
 
@@ -13,25 +9,28 @@ news = on_command("新闻", priority=1, block=True)
 # 添加一个处理函数
 @news.handle()
 async def handle_news(bot: Bot, event: Event, state: dict):
-    # res = requests.get('https://api.apiopen.top/getWangYiNews')
-    # res.encoding = 'utf-8'
-    # res = res.text
-    # cards_data = json.loads(res)
-    # index = 1
-    # dynamic_content = []
-    # if cards_data['code'] == 200:
-    #     results = cards_data['result']
-    #     for result in results:
-    #         path = result['path']
-    #         # image = result['image']
-    #         # title = result['title']
-    #         # passtime = result['passtime']
-    #         # text = ''
-    #         # text += '{0}.{1}({2})\n'.format(index, title, passtime)
-    #         # text += '[CQ:image,file='+image+']'+'\n'
-    #         # text += path
-    #         # # print(text)
-    #         # index += 1
-    #         dynamic_content.append(path)
-    # for content in dynamic_content:
-    await bot.send(event, '不要搞大新闻')
+    
+
+def get_pages():
+    url = 'http://top.baidu.com/buzz?b=1&fr=20811'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    res = requests.get(url, headers=headers)
+    res.encoding = res.apparent_encoding
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+    all_topics = soup.find_all('tr')[1:]
+    news_res = ""
+    for each_topic in all_topics:
+        topic_times = each_topic.find('td', class_='last')  # 搜索指数
+        topic_rank = each_topic.find('td', class_='first')  # 排名
+        topic_name = each_topic.find('td', class_='keyword')  # 标题
+        if topic_rank != None and topic_name != None and topic_times != None:
+            topic_rank = each_topic.find('td', class_='first').get_text().replace(
+                ' ', '').replace('\n', '')
+            topic_name = each_topic.find('td', class_='keyword').get_text().replace(
+                ' ', '').replace('\n', '')[:-6]
+            topic_times = each_topic.find(
+                'td', class_='last').get_text().replace(' ', '').replace('\n', '')
+            news_res += '{}.{}({})'.format(
+                topic_rank, topic_name, topic_times) + '\n'
+    return news_res
